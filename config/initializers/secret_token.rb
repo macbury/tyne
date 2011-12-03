e64efa9ebf3d3eb8406059419e444cc02d602521
= <=>
2011-12-03 12:16:09 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tyne::Application.config.secret_token = '25abf1c48562fb31c17d345e18f7bc3d538b42055e36d2542b22f0cb09281d8f8bb102efbaf55ac4f1443e0cbb932efe1653a3ed7d31b359355398191e5dcd23'
