5769c02c1dc5638a376285b117b8d9429cbb8004
= <=>
2011-12-03 22:54:17 +0000

    fixes display error in fields

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,9 @@ module Tyne
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+      %{<span class="field_with_errors">#{html_tag}</span>}.html_safe
+    end
   end
 end
