d53cea4d6004067d45eeb22eb84e393cb9e9b7db
Tobias Haar <tobias.haar@googlemail.com>
2012-01-10 16:15:29 +0100

    spec for new field story points

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Ticket do
+  describe :fields do
+    it { should have_db_column :story_points }
+  end
+
   describe :validations do
     it { should validate_presence_of :title }
     it { should validate_presence_of :ticket_type_id }
